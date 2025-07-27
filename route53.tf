01366c33d2d8419fe81dbeb2978506f5f50ab190
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-07-27 17:03:16 +0900

    add:alias

--- route53.tf
+++ route53.tf
@@ -2,3 +2,16 @@
 resource "aws_route53_zone" "main" {
   name = local.domain
 }
+
+# エイリアスレコードの作成
+resource "aws_route53_record" "api" {
+  name                             = "api.${local.domain}"
+  type                             = "A"
+  zone_id                          = aws_route53_zone.main.zone_id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+  }
+}
